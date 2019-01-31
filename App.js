3e0b0e72b8011aed50d818e8ee6e4fb17a0d7960
FMtz <fguerrero@apu.edu>
2019-01-30 17:40:04 -0800

    someNewScreens

--- App.js
+++ App.js
@@ -7,10 +7,15 @@ import thunk from 'redux-thunk';
 import reducers from './src/reducers';
 import LoginScreen from './src/screens/LoginScreen';
 import CreateUserScreen from './src/screens/CreateUserScreen';
+import AccountInfoScreen from './src/screens/AccountInfoScreen';
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
 import DashboardScreen from './src/screens/DashboardScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import ExploreScreen from './src/screens/ExploreScreen';
 import NotificationsScreen from './src/screens/NotificationsScreen';
+
+
+
 
 export default class App extends React.Component {
   render() {
@@ -19,6 +24,8 @@ export default class App extends React.Component {
       {
         login: LoginScreen,
         createuser: CreateUserScreen,
+        accountinfo: AccountInfoScreen,
+        forgotpassword: ForgotPasswordScreen,
         main: createBottomTabNavigator(
           {
             dash: DashboardScreen,
--- src/screens/CreateUserScreen.js
+++ src/screens/CreateUserScreen.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class CreateUserScreen extends React.Component {
+  onCancelPress() {
+    this.props.navigation.navigate('login');
+  }
+  /* onConfirmPress(){
+    this.props.navigation.navigate('accountinfo');
+  }  */
+  
+  
   render () {
     return (
       <View>
@@ -13,6 +22,19 @@ class CreateUserScreen extends React.Component {
           CreateUserScreen
           CreateUserScreen
         </Text>
+        <TextInput placeholder='First Name:' />
+        <TextInput placeholder='Last Name:' />
+        <TextInput placeholder='Email:' />
+        <TextInput placeholder='Password:' />
+        <TextInput placeholder='Confirm Password:' />
+
+        
+        <Button
+            title='Cancel / ?????Continue??????'
+            onPress={this.onCancelPress.bind(this)}
+        />
+        
+        
       </View>
     )
   }
