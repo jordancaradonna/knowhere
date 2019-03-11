76dd341153bce845b7f8fd2c5e39ae8e30f69f25
FMtz <fguerrero@apu.edu>
2019-03-11 12:16:12 -0700

    switch branch

--- App.js
+++ App.js
@@ -51,7 +51,7 @@ export default class App extends React.Component {
 
     const MainNavigator = createStackNavigator(
     {
-        
+        accountinfo: AccountInfoScreen,
         dashOutings: DashboardOutingScreen,
         login: LoginScreen,
         createuser: CreateUserScreen,
