2bbb3f31fb68292994c60e8cedfa56bdb436c2ed
aalcos14 <46761206+aalcos14@users.noreply.github.com>
2019-03-19 15:38:29 -0700

    fixed merging errrrr i guess

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,97 +1,148 @@
-import React from 'react';
-import { View, Text, Dimensions } from 'react-native';
-import { Button, Tile, Avatar, ThemeProvider} from 'react-native-elements';
-
+import React, {Component} from 'react';
+import { View, Text, Dimensions, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
+import { Button, Tile, Avatar} from 'react-native-elements';
+import Modal from 'react-native-modal';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
-
-
 class ProfileScreen extends React.Component {
 
+// Buttons to navigate to each branch
+onSettingsPress() {
+  this.props.navigation.navigate('settings');
+}
 
-  onSettingsPress() {
-    this.props.navigation.navigate('settings');
-  }
-  onDreamListPress(){
-    this.props.navigation.navigate('dreamlist');
-  }
-  onTripsPress() {
-    console.log("DOES IT WORK?");
-    this.props.navigation.navigate('trips');
-  }
-  onOutingsPress() {
-    console.log("DOES IT WORK?");
-    this.props.navigation.navigate('outings');
-  }
-  onCreateTripPress() {
-    this.props.navigation.navigate('createtrip');
-  }
-  onCreateOutingPress() {
-    this.props.navigation.navigate('createouting');
-  }
+onCreateTripPress() {
+  this.props.navigation.navigate('createtrip')
+  this.setState({ visibleModal: null });
+}
+
+onCreateOutingPress() {
+  this.props.navigation.navigate('createouting');
+  this.setState({ visibleModal: null });
+}
+
+onDreamListPress(){
+  this.props.navigation.navigate('dreamlist');
+}
+
+state = {
+  visibleModal: null,
+};
+
+// button rendering for modal
+_renderButton = (text, onPress) => (
+  <TouchableOpacity onPress={onPress}>
+    <View style={styles2.button}>
+      <Text>{text}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+// modal content for when creating a new post
+_renderModalContent = () => (
+  <View style={styles2.modalContent}>
+
+  <View alignItems='center' fontSize='300'>
+    <Text>New Post</Text>
+    <Text></Text>
+  </View>
+
+    <View style={{flexGrow: 1}} flexDirection='row' justifyContent='center'>
+    
+      {/* navigate to a createTripScreen */}
+      <View>
+        <Button
+                title='Trip'
+                onPress={this.onCreateTripPress.bind(this)}
+                small
+                titleStyle={styles.buttonTitle}
+                buttonStyle={styles.buttonStyle}
+                color='#83b4ff'
+                backgroundColor='#f4f4ff'>
+        </Button>   
+      </View>
+
+      {/* navigate to createOutingScreen */}
+      <View>
+        <Button title='Outing'
+                onPress={this.onCreateOutingPress.bind(this)}
+                small
+                titleStyle={styles.buttonTitle}
+                buttonStyle={styles.buttonStyle}
+                color='#83b4ff'
+                backgroundColor='#f4f4ff'>
+        </Button>
+      </View>
+    </View>
+
+    {/* renders cancel button to close modal */}
+    <View alignItems='center'>
+    {this._renderButton('Cancel', () => this.setState({ visibleModal: null }))}
+    </View>
+  </View>
+);
 
 
   render () {
     return (
-      <View>
-        <Avatar
-          medium
-          rounded
-          source={require('../images/jprofile.png')}
-          containerStyle={styles.AvatarStyle}
-          onPress={() => console.log("Works!")}
-          activeOpacity={0.7}
-          justifyContent='center'
-          alignItems='center'
-        />
+      <View justifyContent='space-between' alignItems=''>
 
-
-        <View // imageSrc={require()}
-            style={styles.TopProfileStyle} >
-
-          <Text style={styles.NameStyle}> User Name</Text>
+        <ImageBackground  source={require('../images/background1.jpg')}
+                          style={styles.coverPhoto}>
+          
+          
+            <View>
+            <View justifyContent='flex-end' style={{flexGrow: 1}} flexDirection='row'>
+            <Avatar
+              large
+              rounded
+              source={require('../images/jprofile.png')}
+              containerStyle={styles.AvatarStyle}
+              onPress={() => console.log("Works!")}
+              activeOpacity={1}
+            ></Avatar>
+            <View>
+              <Button
+                icon={{name: 'settings', color: 'black'}}
+                backgroundColor = 'clear'
+                style = {{padding: 3}}
+                onPress={this.onSettingsPress.bind(this)}>
+              </Button>
+            </View>
+            </View>
+              
+            </View>
+                  
+        <View alignItems='center' justifyContent='top'>
           <Text></Text>
+          <Text style={styles.NameStyle}>User Name</Text>
+        </View>
+
+
+        <View justifyContent='space-around' alignItems='center' flexDirection='row'>
+
+          <View>
+            {this._renderButton('New Post', () => this.setState({ visibleModal: 1 }))}
+          </View>
 
           <Button
-            small
-            rounded
-            title='settings'
-            color='black'
-            backgroundColor = '#f8f8f8'
-            style = {{padding: 5}}
-            onPress={this.onSettingsPress.bind(this)}
-          />
-          <Button
-            small
-            rounded
-            title='myMap'
-            color='black'
-            backgroundColor = '#f8f8f8'
-            style = {{padding: 5}}
-          />
-
-          <Button
+            buttonStyle={styles.buttonStyle}
             small
             rounded
             title='Dream List'
             color='black'
             backgroundColor = '#f8f8f8'
-            style = {{padding: 5}} 
-            onPress={this.onDreamListPress.bind(this) } />
+            // style = {{padding: 5}} 
+            onPress={this.onDreamListPress.bind(this)}
+          />
 
-          
-
-          <Text></Text>
-          <Text></Text>
-          <Text></Text>
-          <Text></Text>
-        </View>
-
-
-
+        <Modal isVisible={this.state.visibleModal === 1}>
+          {this._renderModalContent()}
+        </Modal>
+        </View>          
      
-        <View style={styles.container2}> 
+        <View flexDirection='row'> 
 
           <View style={styles.buttonContainer}> 
             <Button
@@ -100,9 +151,7 @@ class ProfileScreen extends React.Component {
               titleStyle={styles.buttonTitle}
               buttonStyle={styles.buttonStyle}
               color='#83b4ff'
-              backgroundColor='#f4f4ff' 
-              onPress={this.onTripsPress.bind(this)}
-               />
+              backgroundColor='#f4f4ff'/>
            </View>
         <View style={styles.buttonContainer}> 
             <Button
@@ -111,55 +160,26 @@ class ProfileScreen extends React.Component {
               titleStyle={styles.buttonTitle}
               buttonStyle={styles.buttonStyle}
               color='white'
-              backgroundColor='#b0d0ff'
-              onPress={this.onOutingsPress.bind(this)}
-               />
+              backgroundColor='#b0d0ff'/>
            </View>
         </View>
 
-
-        <Text> </Text>
-        <Text> </Text>
-        <Text> </Text>
-        <Text> </Text>
-      
-        <Button
-            small
-            rounded
-            title='Create Trip'
-            color='black'
-            backgroundColor = '#f8f8f8'
-            style = {{padding: 5}} 
-            onPress={this.onCreateTripPress.bind(this) } />
-
-          <Button
-            small
-            rounded
-            title='Create Outing'
-            color='black'
-            backgroundColor = '#f8f8f8'
-            style = {{padding: 5}} 
-            onPress={this.onCreateOutingPress.bind(this) } />
-
-
-
-
+        </ImageBackground>
 
       </View>
-
-      
+     
     )
     
   }
 }
 
-
-
 const styles = {
   AvatarStyle: {
     marginTop: 20,
-    marginBottom: 20,
-    marginLeft: 40,
+    marginBottom: 22,
+  },
+  leftStyle: {
+    marginLeft: "auto"
   },
   DividerStyle: {
     marginTop: 10,
@@ -167,26 +187,27 @@ const styles = {
   },
   TopProfileStyle: {
     width: SCREEN_WIDTH,
-    height: 150,
+    // height: 150,
     alignItems: 'center',
-    justifyContent: 'center'
+    // flex: 2,
+    flexDirection: 'row',
   },
   NameStyle: {
     fontFamily: 'Al Nile',
     fontSize: 18,
-    color: 'black'
+    color: 'black',
+    justifyContent: 'center'
   },
   container2:{
     flex: 1,
     flexDirection: 'row',
-    //alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'bottom',
   },
   buttonContainer:{
     flex: 1,
-    marginLeft: 25,
+    marginLeft: 8,
     marginRight: 25,
-
   },
   buttonTitle:{
     fontSize: 16,
@@ -198,7 +219,40 @@ const styles = {
     //padding: 20,
     borderRadius: 100, //makes it oval not squared
     elevation: 5,
-  }
-};
+  },
+  coverPhoto:{
+    width: '100%',
+    height: '63%'
+  }};
+
+  const styles2 = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    button: {
+      width: 115,
+      height: 44,
+      elevation: 5,
+      backgroundColor: 'lightblue',
+      padding: 12,
+      margin: 16,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 100,
+      borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    modalContent: {
+      backgroundColor: 'white',
+      padding: 22,
+      borderRadius: 22,
+      borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    bottomModal: {
+      justifyContent: 'flex-end',
+      margin: 0,
+    },
+  });
 
 export default ProfileScreen;
