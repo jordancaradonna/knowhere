0b2e12d0951d10904ddd78e9645ed27839b66d97
Jordan  Caradonna <jcaradonna15@apu.edu>
2019-03-22 12:09:11 -0700

    map-feature phase 1

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -1,22 +1,75 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import MapView, { Marker } from 'react-native-maps';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 class NotificationsScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
-  render () {
+    this.state = {
+      markers: []
+    }
+    this.handlePress = this.handlePress.bind(this);
+  }
+  handlePress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          cost: `$${getRandomInt(50, 300)}`
+        }
+      ]
+    })
+  }
+  render() {
     return (
-      <View>
-        <Text>
-          NotificationsScreen
-          NotificationsScreen
-          NotificationsScreen
-          NotificationsScreen
-          NotificationsScreen
-          NotificationsScreen
-        </Text>
-      </View>
-    )
+      <MapView 
+        style={styles.container}
+        initialRegion={{
+            latitude: 45.5209087,
+            longitude: -122.6705107,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }} 
+          onPress={this.handlePress}
+      >
+      {this.state.markers.map((marker) => {
+        return (
+          <Marker {...marker} />
+            
+        )
+      })}
+      </MapView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  marker: {
+    backgroundColor: "#550bbc",
+    padding: 5,
+    borderRadius: 5,
+  },
+  text: {
+    color: "#FFF",
+    fontWeight: "bold"
+  }
+});
 
 export default NotificationsScreen;
