ebfde4a4b139b2ef99ea6deac8c96ee8e2b0e9ed
Jordan  Caradonna <jcaradonna15@apu.edu>
2019-03-22 12:24:53 -0700

    committed map feature to notification in testing branch

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -1,52 +1,72 @@
-import React from 'react';
-import { View, Text, StyleSheet,SafeAreaView } from 'react-native';
-import { Button } from 'react-native-elements';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
+import MapView, { Marker } from 'react-native-maps';
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 class NotificationsScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
-  render () {
+    this.state = {
+      markers: []
+    }
+    this.handlePress = this.handlePress.bind(this);
+  }
+  handlePress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+        }
+      ]
+    })
+  }
+  render() {
     return (
-      <SafeAreaView style={{flex:1}}>
-      <View style={[styles1.container,]}>
-        <Text style={[styles1.title,]}>
-          Notifications
-        </Text>
-
-      </View>
-      </SafeAreaView>
-    )
+      <MapView 
+        style={styles.container}
+        initialRegion={{
+            latitude: 45.5209087,
+            longitude: -122.6705107,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }} 
+          onPress={this.handlePress}
+      >
+      {this.state.markers.map((marker) => {
+        return (
+          <Marker {...marker} />
+        )
+      })}
+      </MapView>
+    );
   }
 }
 
-const styles1 = StyleSheet.create({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    alignItems: 'center',
   },
-  container2:{
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  title:{
-    fontSize: 28,
-    //fontFamily: 'Chalkboard SE',
+  marker: {
+    backgroundColor: "#550bbc",
     padding: 5,
-    color: 'black',
+    borderRadius: 5,
   },
-  buttonTitle:{
-    fontSize: 28,
-    letterSpacing: 2,
-    //color: "white",
-  },
-  buttonStyle:{
-    //width: 350,
-    marginLeft: 0.0,
-    marginRight:0.0,
-    borderColor: "grey",
-    //borderRadius: 100, //makes it oval not squared
-    //elevation: 5,
+  text: {
+    color: "#FFF",
+    fontWeight: "bold"
   }
 });
 
