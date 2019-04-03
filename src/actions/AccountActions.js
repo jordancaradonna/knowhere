9c6788ab3adf326c2dbcaeee5aef2fe3a2e237fe
Chad Ditter <cditter14@apu.edu>
2019-04-02 21:26:43 -0700

    Got the boiler for updating photos but it is still not functional

--- src/actions/AccountActions.js
+++ src/actions/AccountActions.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { RNS3 } from 'react-native-aws3';
 import {
   FNAME_CHANGED,
   LNAME_CHANGED,
@@ -42,11 +43,36 @@ export const photoChanged = (image) => {
   };
 };
 
-export const createProfile = ({fname, lname, username, city}, callbackFunction ) => {
+export const createProfile = ({fname, lname, username, city, photo}, callbackFunction ) => {
   const currentUser = firebase.auth().O;
-
+  console.log('HERE HERE HERE', photo);
   return (dispatch) => {
     dispatch({ type: CREATE_PROFILE });
+    const file = {
+      uri: photo.uri,
+      name: 'profile_pic.jpeg',
+      type: 'image/jpeg',
+    };
+
+    const options = {
+      keyPrefix: 'images/',
+      bucket: 'knowhere-pictures',
+      region: 'us-east-1',
+      accessKey: 'AKIAJMD4JHWXJR2D25HA',
+      secretKey: '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',
+      successActionStatus: 201
+    };
+
+    RNS3.put(file, options).then(response => {
+      if (response.status != 201) {
+        throw new Error('Failed to upload image to s3', response);
+      }
+      console.log('***Body***', response.body);
+    })
+    .catch((error) => {
+      console.log(error);
+      createProfileFail(dispatch, error.message);
+    });
     firebase.database().ref(`users/${currentUser}/account`)
       .set({ fname, lname, username, city })
       .then(user => createProfileSuccess(dispatch, callbackFunction))
@@ -59,7 +85,7 @@ export const createProfile = ({fname, lname, username, city}, callbackFunction )
 
 const createProfileFail = (dispatch, errorText) => {
   dispatch({
-    type: CREATE_PROFILE_FAIL, 
+    type: CREATE_PROFILE_FAIL,
     payload: errorText
   });
 };
--- src/reducers/AccountInfoReducer.js
+++ src/reducers/AccountInfoReducer.js
@@ -23,29 +23,22 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case FNAME_CHANGED:
-      console.log(action.payload);
       return { ...state, fname: action.payload };
     case LNAME_CHANGED:
-      console.log(action.payload);
       return { ...state, lname: action.payload };
     case USERNAME_CHANGED:
-      console.log(action.payload);
       return { ...state, username: action.payload };
     case CITY_CHANGED:
-      console.log(action.payload);
       return { ...state, city: action.payload };
     case PHOTO_CHANGED:
-      console.log(action.payload);
       return { ...state, photo: action.payload };
     case CREATE_PROFILE:
-      console.log('creating');
       return { ...state, loading: true, error: '' };
     case CREATE_PROFILE_SUCCESS:
       return { ...state, ...INITIAL_STATE };
     case CREATE_PROFILE_FAIL:
       return { ...state, error: action.payload, loading: false };
     case PROFILE_FETCH_SUCCESS:
-      console.log(action.payload);
       return { ...state,
         city: action.payload.city,
         username: action.payload.username,
