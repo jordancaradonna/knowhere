dd1e41e08cd8e37cc3dacd9e451b5604e03f2042
FMtz <fguerrero@apu.edu>
2019-04-03 12:17:38 -0700

    aidan's branch

--- src/screens/CreateTripScreen.js
+++ src/screens/CreateTripScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Switch, ScrollView} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { Button } from 'react-native-elements';
 import ImagePicker from 'react-native-image-picker';
 
 class CreateTripScreen extends React.Component {
+
+  //------------------STATE PROPERTIES FOR CHOOSE PHOTO------------------//
 
   state = {
     photo: null,
@@ -24,10 +27,22 @@ class CreateTripScreen extends React.Component {
     this.props.navigation.navigate('dash')
   }
 
-  
+  //------------------STATE PROPERTIES FOR SWITCH----------------//
+  //Initial state false for the switch. You can change it to true just to see.
+  state = { switchValue: false };
+
+  toggleSwitch = value => {
+    //onValueChange of the switch this function will be called
+    this.setState({ switchValue: value });
+    //state changes according to switch
+    //which will result in re-render the text
+  };
+
   render () {
-    const { photo } = this.state
+    const { photo } = this.state;
     return (
+      
+      <ScrollView>
       <View style={{justifyContent: "space-evenly"}}>
         <Text style={{ fontSize: 20, alignSelf: 'center', padding: 20}}>
           Create Trip
@@ -38,39 +53,81 @@ class CreateTripScreen extends React.Component {
             style={{ width: 300, height: 300 }}
           />
         )}
-        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
-        <TextInput style={{ height: 160, width: 200,
-                            backgroundColor: 'white', 
-                            borderColor: '#ffffff', 
-                            borderWidth: 0.5, }}
-          placeholder='Trip Description'
-          editable = {true}
-          multiline = {true}
-          onChangeText={(text) => this.setState({text})}
-        />
-        <TextInput style={{ height: 40, width: 200,
-                            backgroundColor: 'white', 
-                            borderColor: '#ffffff', 
-                            borderWidth: 0.5, }}
-          placeholder='Location'
-        />
-        <TextInput style={{ height: 40, width: 200,
-                            backgroundColor: 'white', 
-                            borderColor: '#ffffff', 
-                            borderWidth: 0.5, }}
-          placeholder='Tag People'
-        />
         <Button
-          title='Post Trip'
+          title="Choose Photo"
+          iconRight={{name:'photo', color:'black'}}
+          onPress={this.handleChoosePhoto}
           titleStyle={styles.buttonTitle}
           buttonStyle={styles.buttonStyle}
-          onPress={this.onPostPress.bind(this)}
-          justifyContent='center'
         />
+
+        {/* ============== SCROLLABLE VIEW WHEN KEYBOARD IS ACTIVE ============ */}
+        <KeyboardAwareScrollView>
+
+          {/* ======== TEXT INPUT FOR DESCRIPTION OF TRIP/OUTING ========= */}
+          <TextInput style={{ height: 160, width: 200,
+                              backgroundColor: 'white', 
+                              borderColor: '#ffffff', 
+                              borderWidth: 5, }}
+            placeholder='Trip Description'
+            editable = {true}
+            blurOnSubmit
+            multiline = {true}
+            onChangeText={(text) => this.setState({text})}
+          />
+
+          {/* =========TEXT INPUT FOR LOCATION======= */}
+          <TextInput 
+            style={{ height: 40, width: 200,
+                    backgroundColor: 'white', 
+                    borderColor: '#ffffff', 
+                    borderWidth: 5, }}
+            placeholder='Location'
+            blurOnSubmit
+          />
+
+          {/*-----------INPUT FOR TAGGING OTHER USERS IN YOUR TRIP/OUTING------------*/}
+        
+          <TextInput
+            style={{ height: 40, width: 200,
+                    backgroundColor: 'white', 
+                    borderColor: '#ffffff', 
+                    borderWidth: 5, }}
+            placeholder='Tag People'
+            blurOnSubmit
+          />
+        </KeyboardAwareScrollView>     
+
+        <View flexDirection='row' justifyContent='center' alignItems='center'>
+
+          {/*-----------SWITCH FOR TOGGLING BETWEEN PUBLIC AND PRIVATE POSTING--------*/}
+          <Text>{this.state.switchValue ? 'Private' : 'Public'}</Text>
+          <Switch
+            style={{ marginTop: 10, marginBottom: 10 }}
+            onValueChange={this.toggleSwitch}
+            value={this.state.switchValue}
+          />
+          <Text></Text>
+
+          {/*-----------PUBLISH POST BUTTON------------*/}
+        
+          <Button
+            title='Publish Post'
+            titleStyle={styles.buttonTitle}
+            buttonStyle={styles.buttonStyle}
+            onPress={this.onPostPress.bind(this)}
+            justifyContent='center'
+          />
+        </View>
+         
       </View>
+      </ScrollView>
+      
     )
   }
 }
+
+
 
 const styles = StyleSheet.create({
   container:{
@@ -101,12 +158,17 @@ const styles = StyleSheet.create({
     color: "white",
   },
   buttonStyle:{
-    //width: 90,
+    width: 130,
     height: 42,
     backgroundColor: "#83b4ff",
     borderRadius: 100, //makes it oval not squared
     elevation: 5,
-  }
+    marginBottom: 5
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
 });
 
 export default CreateTripScreen;
--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -1,60 +1,57 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-
-import MapView, { Marker } from 'react-native-maps';
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+import {AppRegistry, StyleSheet, Text, View, SectionList} from 'react-native';
 
 class NotificationsScreen extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      markers: []
-    }
-    this.handlePress = this.handlePress.bind(this);
-  }
-  handlePress(e) {
-    this.setState({
-      markers: [
-        ...this.state.markers,
-        {
-          coordinate: e.nativeEvent.coordinate,
-        }
-      ]
-    })
-  }
   render() {
     return (
-      <View> 
-        
-      </View>
+        <View  style={styles.container}>
+        <Text style={[styles.title,]}> Notifications </Text>
+          <SectionList
+            
+            sections={[
+              {title: 'Today', data: ['Devin']},
+              {title: 'Yesterday', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+            ]}
+            renderItem={({item}) => <Text style={styles.item}>{item} Saved your post</Text>}
+            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            keyExtractor={(item, index) => index}
+          />
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+   flex: 1,
+   paddingTop: 22,
+   alignItems: 'center'
   },
-  marker: {
-    backgroundColor: "#550bbc",
-    padding: 5,
-    borderRadius: 5,
+    container2:{
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    title:{
+      fontSize: 28,
+      //fontFamily: 'Chalkboard SE',
+      padding: 5,
+      color: 'black',
+    },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'lightblue',
   },
-  text: {
-    color: "#FFF",
-    fontWeight: "bold"
-  }
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
 });
 
 export default NotificationsScreen;
