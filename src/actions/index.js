0f047892ff7f06895748de20290593aa6ac3546b
Chad Ditter <cditter14@apu.edu>
2019-05-01 03:56:16 -0700

    sourcetree sucks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,5 @@
 export * from './AuthActions';
 export * from './AccountActions';
-<<<<<<< HEAD
 export * from './ExploreActions';
-=======
 export * from './OutingTripActions';
 export * from './FeedActions';
->>>>>>> Development/16-CreateOuting
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,14 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import AccountInfoReducer from './AccountInfoReducer';
-<<<<<<< HEAD
 import ExploreSearchReducer from './ExploreSearchReducer';
-=======
 import OutingReducer from './OutingReducer';
 import FeedReducer from './FeedReducer';
->>>>>>> Development/16-CreateOuting
 
 export default combineReducers({
   auth: AuthReducer,
   info: AccountInfoReducer,
-<<<<<<< HEAD
-  explore: ExploreSearchReducer
-
-=======
+  explore: ExploreSearchReducer,
   out: OutingReducer,
   feed: FeedReducer
->>>>>>> Development/16-CreateOuting
 });
